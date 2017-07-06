caf28a6eb9ccfcc857b8c5e83120c6f646b45cef
staff2 <takahamasuzuki@gmail.com>
2017-07-06 17:32:00 +0900

    add 料金案内ページ

--- home/common/inc/globalmenu.php
+++ home/common/inc/globalmenu.php
@@ -1,80 +1,146 @@
 <div id="gnavi">
 	<div class="inner">
 		<ul>
-			<li><img src="/common/img/header/gnavi_01.png" alt="アイテム一覧" class="gnimg"><p class="gntxt">アイテム一覧</p>
+			<li><img src="/common/img/header/gnavi_01.png" alt="アイテム一覧" class="gnimg">
+				<p class="gntxt">アイテム一覧</p>
 				<ul class="items" style="visibility: visible;overflow: visible;">
-				<li class="com">アイテムを選んで注文を開始 ！（Web上でお見積もり可能）</li>
-					<li><a href="/items/t-shirts/" onclick="ga('send','event','tshirts','click','global');"><img src="/common/img/header/gnavi_t-shirts.png" alt="オリジナルTシャツ"><p>Tシャツ</p></a></li>
-					<li><a href="/items/sweat/" onclick="ga('send','event’,'sweat','click','global');"><img src="/common/img/header/gnavi_sweat.png" alt="オリジナルパーカー・オリジナルスウェット"><p>パーカー・スウェット</p></a></li>
-					<li><a href="/items/polo-shirts/" onclick="ga('send','event’,’polo-shirts','click','global');"><img src="/common/img/header/gnavi_polo.png" alt="オリジナルポロシャツ"><p>ポロシャツ</p></a></li>
-					<li><a href="/items/sportswear/" onclick="ga('send','event’,'sportswear','click','global');"><img src="/common/img/header/gnavi_sports.png" alt="オリジナルスポーツウェア"><p>スポーツウェア</p></a></li>
-					<li><a href="/items/long-shirts/" onclick="ga('send','event’,’long-shirts','click','global');"><img src="/common/img/header/gnavi_long-t.png" alt="オリジナルロングTシャツ"><p>ロングTシャツ</p></a></li>
-					<li><a href="/items/outer/" onclick="ga('send','event’,'outer','click','global');"><img src="/common/img/header/gnavi_outer.png" alt="オリジナルジャンパー・オリジナルブルゾン"><p>ジャンパー・ブルゾン</p></a></li>
-					<li><a href="/items/ladys/" onclick="ga('send','event’,'lady','click','global');"><img src="/common/img/header/gnavi_ladys.png" alt="オリジナルレディースウェア"><p>レディースウェア</p></a></li>
-					<li><a href="/items/towel/" onclick="ga('send','event’,'towel','click','global');"><img src="/common/img/header/gnavi_towel.png" alt="オリジナルタオル"><p>タオル</p></a></li>
-					<li><a href="/items/tote-bag/" onclick="ga('send','event’,’tote-bag','click','global');"><img src="/common/img/header/gnavi_tote-bag.png" alt="オリジナルバッグ・オリジナルトートバッグ"><p>バッグ・トートバッグ</p></a></li>
-					<li><a href="/items/apron/" onclick="ga('send','event’,'apron','click','global');"><img src="/common/img/header/gnavi_apron.png" alt="オリジナルエプロン"><p>エプロン</p></a></li>
-					<li><a href="/items/baby/" onclick="ga('send','event’,'baby','click','global');"><img src="/common/img/header/gnavi_baby.png" alt="オリジナルベビーウェア"><p>ベビーウェア</p></a></li>
-					<li><a href="/items/overall/" onclick="ga('send','event’,'overall','click','global');"><img src="/common/img/header/gnavi_overall.png" alt="オリジナルつなぎ"><p>つなぎ</p></a></li>
-					<li><a href="/items/workwear/" onclick="ga('send','event’,'workwear','click','global');"><img src="/common/img/header/gnavi_workwear.png" alt="オリジナルワークウェア"><p>ワークウェア</p></a></li>
-					<li><a href="/items/cap/" onclick="ga('send','event’,'cap','click','global');"><img src="/common/img/header/gnavi_cap.png" alt="オリジナルキャップ・バンダナ"><p>キャップ・バンダナ</p></a></li>
-					<li><a href="/items/goods/" onclick="ga('send','event’,'goods','click','global');"><img src="/common/img/header/gnavi_goods.png" alt="記念品・プレゼント"><p>記念品・プレゼント</p></a></li>
+					<li class="com">アイテムを選んで注文を開始 ！（Web上でお見積もり可能）</li>
+					<li>
+						<a href="/items/t-shirts/" onclick="ga('send','event','tshirts','click','global');"><img src="/common/img/header/gnavi_t-shirts.png" alt="オリジナルTシャツ">
+							<p>Tシャツ</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/sweat/" onclick="ga('send','event’,'sweat','click','global');"><img src="/common/img/header/gnavi_sweat.png" alt="オリジナルパーカー・オリジナルスウェット">
+							<p>パーカー・スウェット</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/polo-shirts/" onclick="ga('send','event’,’polo-shirts','click','global');"><img src="/common/img/header/gnavi_polo.png" alt="オリジナルポロシャツ">
+							<p>ポロシャツ</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/sportswear/" onclick="ga('send','event’,'sportswear','click','global');"><img src="/common/img/header/gnavi_sports.png" alt="オリジナルスポーツウェア">
+							<p>スポーツウェア</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/long-shirts/" onclick="ga('send','event’,’long-shirts','click','global');"><img src="/common/img/header/gnavi_long-t.png" alt="オリジナルロングTシャツ">
+							<p>ロングTシャツ</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/outer/" onclick="ga('send','event’,'outer','click','global');"><img src="/common/img/header/gnavi_outer.png" alt="オリジナルジャンパー・オリジナルブルゾン">
+							<p>ジャンパー・ブルゾン</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/ladys/" onclick="ga('send','event’,'lady','click','global');"><img src="/common/img/header/gnavi_ladys.png" alt="オリジナルレディースウェア">
+							<p>レディースウェア</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/towel/" onclick="ga('send','event’,'towel','click','global');"><img src="/common/img/header/gnavi_towel.png" alt="オリジナルタオル">
+							<p>タオル</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/tote-bag/" onclick="ga('send','event’,’tote-bag','click','global');"><img src="/common/img/header/gnavi_tote-bag.png" alt="オリジナルバッグ・オリジナルトートバッグ">
+							<p>バッグ・トートバッグ</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/apron/" onclick="ga('send','event’,'apron','click','global');"><img src="/common/img/header/gnavi_apron.png" alt="オリジナルエプロン">
+							<p>エプロン</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/baby/" onclick="ga('send','event’,'baby','click','global');"><img src="/common/img/header/gnavi_baby.png" alt="オリジナルベビーウェア">
+							<p>ベビーウェア</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/overall/" onclick="ga('send','event’,'overall','click','global');"><img src="/common/img/header/gnavi_overall.png" alt="オリジナルつなぎ">
+							<p>つなぎ</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/workwear/" onclick="ga('send','event’,'workwear','click','global');"><img src="/common/img/header/gnavi_workwear.png" alt="オリジナルワークウェア">
+							<p>ワークウェア</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/cap/" onclick="ga('send','event’,'cap','click','global');"><img src="/common/img/header/gnavi_cap.png" alt="オリジナルキャップ・バンダナ">
+							<p>キャップ・バンダナ</p>
+						</a>
+					</li>
+					<li>
+						<a href="/items/goods/" onclick="ga('send','event’,'goods','click','global');"><img src="/common/img/header/gnavi_goods.png" alt="記念品・プレゼント">
+							<p>記念品・プレゼント</p>
+						</a>
+					</li>
 				</ul>
 			</li>
-			<li><img src="/common/img/header/gnavi_02.png" alt="納期内容" class="gnimg"><p class="gntxt">納期</p>
+			<li><img src="/common/img/header/gnavi_02.png" alt="納期内容" class="gnimg">
+				<p class="gntxt">納期</p>
 				<ul>
 					<li><a href="/delivery/" onclick="ga('send','event','delivery','click','global');">お届け日を調べる</a></li>
 					<li><a href="/sameday/" onclick="ga('send','event','sameday','click','global');">当日特急プラン！！</a></li>
 					<li><a href="/reason/speed.html" onclick="ga('send','event','sameday','click','global');">お届けが早い理由</a></li>
 				</ul>
 			</li>
-			<li><img src="/common/img/header/gnavi_03.png" alt="料金" class="gnimg"><p class="gntxt">料金</p>
+			<li><img src="/common/img/header/gnavi_03.png" alt="料金" class="gnimg">
+				<p class="gntxt">料金</p>
 				<ul>
 					<li><a href="/price/estimate.html" onclick="ga('send','event','estimate','click','global');">10秒見積もり</a></li>
-					<!--<li><a href="/price/fee/">プリント料金案内</a></li>-->
+					<li><a href="/price/fee/">プリント料金案内</a></li>
 					<li><a href="/guide/#payment">お支払い方法・送料</a></li>
 					<li><a href="/guide/bring.html">持ち込みについて</a></li>
 					<li><a href="/guide/#discount">割引プラン</a></li>
 				</ul>
 			</li>
-			<li><img src="/common/img/header/gnavi_04.png" alt="オリジナルデザイン" class="gnimg"><p class="gntxt">デザイン</p>
+			<li><img src="/common/img/header/gnavi_04.png" alt="オリジナルデザイン" class="gnimg">
+				<p class="gntxt">デザイン</p>
 				<ul class="wide">
 					<li><a href="/design/designtemp.html" onclick="ga('send','event','designtemp','click','global');">無料！デザイン素材集</a></li>
 					<li><a href="/design/designguide.html" onclick="ga('send','event','designguide','click','global');">デザインの入稿・作り方</a></li>
 					<li><a href="/design/printing.html" onclick="ga('send','event','printing','click','global');">プリント方法の種類</a></li>
 					<li><a href="/design/emb.html" onclick="ga('send','event','emb','click','global');">刺繍・名入れサービス</a></li>
-                    <li><a href="/design/printsize.html" onclick="ga('send','event','printing','click','global');">プリントサイズ</a></li>
+					<li><a href="/design/printsize.html" onclick="ga('send','event','printing','click','global');">プリントサイズ</a></li>
 					<li><a href="/design/designposition.html" onclick="ga('send','event','designposition','click','global');">プリント位置について</a></li>
 					<li><a href="/design/fontcolor.html" onclick="ga('send','event','fontcolor','click','global');">インクカラーとフォント種類</a></li>
 					<li><a href="/design/gallery.html" onclick="ga('send','event','gallery','click','global');">デザイン実例</a></li>
 					<li><a href="/design/support/" onclick="ga('send','event','support','click','global');">デザインサポート</a></li>
-					
+
 					<li><a href="/design/template_illust.html" onclick="ga('send','event','template_illust','click','global');">イラレ入稿テンプレート</a></li>
 					<li><a href="/design/designguide_freehand.html" onclick="ga('send','event','designguide_freehand','click','global');">手描き入稿用紙（FAX）</a></li>
 				</ul>
 			</li>
-            
-            <li id="odrbtn"><img src="/common/img/header/gnavi_05.png" alt="申し込み" class="gnimg"><p class="gntxt">申し込み</p>
-                <ul>
-                    <li id="odrtxt"><a href="/order/" onclick="ga('send','event','order','click','global');">申し込み</a></li>
-                    <li><a href="/guide/orderflow.html" onclick="ga('send','event','orderflow','click','global');">ご利用の流れ</a></li>
-                    <li><a href="/order/reorder.html" onclick="ga('send','event','reorder','click','global');">追加・再注文の方法</a></li>
-                </ul>
-            </li>
 
-            <li class="adjust"><img src="/common/img/header/gnavi_06.png" alt="オリジナルTシャツ屋について" class="gnimg"><p class="gntxt">タカハマについて</p>
-                <ul>
-                    <li><a href="/reason/" onclick="ga('send','event','reason','click','global');">選ばれる理由</a></li>
-                    <li><a href="/userreviews/" onclick="ga('send','event','userreviews','click','global');">レビュー・評価</a></li>
-                    <li><a href="/blog/thanks_blog/" onclick="ga('send','event','thanks_blog','click','global');">お客様ブログ</a></li>
-                    <li><a href="/app/WP/?cat=4" onclick="ga('send','event','staff-blog','click','global');">スタッフブログ</a></li>
-                    <li><a href="/corporate/profile/staff.html" onclick="ga('send','event','staff','click','global');">スタッフ紹介</a></li>
-                    <li><a href="/corporate/profile/manager.html" onclick="ga('send','event','manager','click','global');">代表挨拶</a></li>
-                    <li><a href="/corporate/overview.html" onclick="ga('send','event','overview','click','global');">会社概要</a></li>
-                </ul>
-            </li>
+			<li id="odrbtn"><img src="/common/img/header/gnavi_05.png" alt="申し込み" class="gnimg">
+				<p class="gntxt">申し込み</p>
+				<ul>
+					<li id="odrtxt"><a href="/order/" onclick="ga('send','event','order','click','global');">申し込み</a></li>
+					<li><a href="/guide/orderflow.html" onclick="ga('send','event','orderflow','click','global');">ご利用の流れ</a></li>
+					<li><a href="/order/reorder.html" onclick="ga('send','event','reorder','click','global');">追加・再注文の方法</a></li>
+				</ul>
+			</li>
 
-            
+			<li class="adjust"><img src="/common/img/header/gnavi_06.png" alt="オリジナルTシャツ屋について" class="gnimg">
+				<p class="gntxt">タカハマについて</p>
+				<ul>
+					<li><a href="/reason/" onclick="ga('send','event','reason','click','global');">選ばれる理由</a></li>
+					<li><a href="/userreviews/" onclick="ga('send','event','userreviews','click','global');">レビュー・評価</a></li>
+					<li><a href="/blog/thanks_blog/" onclick="ga('send','event','thanks_blog','click','global');">お客様ブログ</a></li>
+					<li><a href="/app/WP/?cat=4" onclick="ga('send','event','staff-blog','click','global');">スタッフブログ</a></li>
+					<li><a href="/corporate/profile/staff.html" onclick="ga('send','event','staff','click','global');">スタッフ紹介</a></li>
+					<li><a href="/corporate/profile/manager.html" onclick="ga('send','event','manager','click','global');">代表挨拶</a></li>
+					<li><a href="/corporate/overview.html" onclick="ga('send','event','overview','click','global');">会社概要</a></li>
+				</ul>
+			</li>
+
+
 
 		</ul>
 	</div>
